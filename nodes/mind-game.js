9240c8a869fe8d7ddc8f589f7f2ecf4ae9d1c456
winston_hsieh <winston_hsieh@gemteks.com>
2017-04-13 21:29:49 +0800

    Implement the choose mind game nod

--- nodes/mind-game.js
+++ nodes/mind-game.js
@@ -1,11 +1,30 @@
+var settings = require('../bluemix-settings.js'),
+    nano = require('nano')(settings.couchUrl);
+
 module.exports = function(RED) {
     function chooseMindGameNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         this.on('input', function(msg) {
-            // convert to lower case
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+
+            var minedb = nano.db.use('mind_questions');
+            minedb.list(function(err, body) {
+
+                if (err) {
+                    node.warn(err);
+                }
+
+                Math.random() * (max - 0) + min;
+
+                if (body.length > 0) {
+                    var index = getRandomInt(0, body.length - 1);
+                    msg.payload = body[index];
+                } else {
+                    msg.payload = null;
+                }
+
+                node.send(msg);
+            });
         });
     }
     RED.nodes.registerType("choose-mind-game", chooseMindGameNode);
@@ -20,4 +39,12 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("analysis-mind-game", analysisMindGameNode);
+
+    /**
+     * Returns a random integer between min (inclusive) and max (inclusive)
+     * Using Math.round() will give you a non-uniform distribution!
+     */
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 }
